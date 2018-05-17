87b0cb0c6883f9e641d6e0eeaf181097b4895667
Arman Rahman <arrahm@microsoft.com>
2018-05-17 18:35:59 -0400

    minimized try catch block and added log4j

--- src/main/java/com/github/catalystcode/clausieserver/ClausIEJsonlProcessor.java
+++ src/main/java/com/github/catalystcode/clausieserver/ClausIEJsonlProcessor.java
@@ -28,20 +28,15 @@ class ClausIEJsonlProcessor {
 
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(instream))) {
             String line;
-            try{
-                while ((line = reader.readLine()) != null) {
-                    Collection<ClausIERelation> relations = parser.parse(line);
-                    if (relations != null) {
-                        for (ClausIERelation relation : relations) {
-                            String json = formatter.format(relation);
-                            outstream.write(json.getBytes(UTF_8));
-                            outstream.write(NEWLINE_BYTES);
-                        }
+            while ((line = reader.readLine()) != null) {
+                Collection<ClausIERelation> relations = parser.parse(line);
+                if (relations != null) {
+                    for (ClausIERelation relation : relations) {
+                        String json = formatter.format(relation);
+                        outstream.write(json.getBytes(UTF_8));
+                        outstream.write(NEWLINE_BYTES);
                     }
                 }
-            }
-            catch (IOException e) {
-                throw new RuntimeException(e);
             }
         }
     }
--- src/main/java/com/github/catalystcode/clausieserver/ClausIEParser.java
+++ src/main/java/com/github/catalystcode/clausieserver/ClausIEParser.java
@@ -7,11 +7,13 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import org.apache.log4j.Logger;
 
 import static java.util.Collections.emptyList;
 
 
 class ClausIEParser {
+    private static final Logger logger = Logger.getLogger(ClausIEParser.class);
     private final ClausIE clausIE;
     private final AtomicInteger lineCounter;
 
@@ -27,40 +29,40 @@ class ClausIEParser {
             return emptyList();
         }
 
+        List<Proposition> propositions = new ArrayList<>();
+        
         try {
             clausIE.parse(sentence);
             clausIE.detectClauses();
             clausIE.generatePropositions();
+            propositions = clausIE.getPropositions();
+        } catch (StackOverflowError e) {
+            logger.error("Error parsing sentence: " + sentence);
+        }     
+        
+        List<ClausIERelation> relations = new ArrayList<>(propositions.size());
 
-            List<Proposition> propositions = clausIE.getPropositions();
-            List<ClausIERelation> relations = new ArrayList<>(propositions.size());
-
-            for (Proposition proposition : propositions) {
-                int numArguments = proposition.noArguments();
-                if (numArguments == 0) {
-                    continue;
-                }
-
-                String subject = proposition.subject();
-                String relation = proposition.relation();
-                String argument = proposition.argument(0);
-
-                List<String> extraArguments = numArguments > 1
-                    ? new ArrayList<>(numArguments - 1)
-                    : emptyList();
-
-                for (int i = 1; i < numArguments; i++) {
-                    extraArguments.add(proposition.argument(i));
-                }
-
-                relations.add(new ClausIERelation(lineNumber, subject, relation, argument, extraArguments));
+        for (Proposition proposition : propositions) {
+            int numArguments = proposition.noArguments();
+            if (numArguments == 0) {
+                continue;
             }
 
-            return relations;
-            
-        } catch (Exception e) {
-            System.out.println("Error parsing sentence: " + sentence);
+            String subject = proposition.subject();
+            String relation = proposition.relation();
+            String argument = proposition.argument(0);
+
+            List<String> extraArguments = numArguments > 1
+                ? new ArrayList<>(numArguments - 1)
+                : emptyList();
+
+            for (int i = 1; i < numArguments; i++) {
+                extraArguments.add(proposition.argument(i));
+            }
+
+            relations.add(new ClausIERelation(lineNumber, subject, relation, argument, extraArguments));
         }
-        return null;
+
+        return relations;     
     }
 }
